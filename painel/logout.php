1a305a16dcd0e1fc09681259d59b58f403ca5491
Leonan Carvalho <j.leonancarvalho@gmail.com>
2015-07-31 11:31:28 -0300

    Commit inicial dos arquivos do VoteSystem
    Conteúdo extraido do .rar da versão 2.6

---
+++ painel/logout.php
@@ -0,0 +1,14 @@
+<?php
+##########################################
+#				 Créditos:				 #
+#	Este sistema foi desenvolvido por:	 #
+#		 Ivan Pires (ICPNetworks)		 #
+#		    E estilizado por:			 #
+#		Hugo Felipe (ICPNetworks)		 #
+#	E-mail: contato@icpnetworks.com.br	 #
+#	Site: http://www.icpnetworks.com.br	 #
+##########################################
+	session_start();
+	session_destroy();
+?>
+<BODY onLoad="document.location.href='../'">
