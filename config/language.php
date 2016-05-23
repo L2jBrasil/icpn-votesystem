b75b25cffdc050587c633f6cced911e6869d4721
José Leonan S. Carvalho <j.leonancarvalho@gmail.com>
2016-05-23 20:05:36 -0300

    /Correção acesso top l2jbrasil

--- config/language.php
+++ config/language.php
@@ -225,4 +225,68 @@ $language_68 = 'No';
 $language_69 = 'Error in the delivery of the prize, inform the board administrator.';
 }
 $version = '2.6';
+
+if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_X_FORWARDED_FOR"];
+}
+ 
+if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CLIENT_IP"];
+}
+ 
+ 
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
+
+
+// Correção acesso top l2jbrasil
+
+function checkOnline($domain) {
+   $ch = curl_init($domain);
+   curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,10);
+   curl_setopt($ch,CURLOPT_HEADER,true);
+   curl_setopt($ch,CURLOPT_NOBODY,false);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   curl_setopt($ch,CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT'] );
+   curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+
+   //get answer
+   
+   $response = curl_exec($ch);
+   curl_close($ch);
+   if ($response) return true;
+   return false;
+}
+
+function acessoSimples($url, &$info = null, $get= array() , $post=array(), $timeout = 10) {
+	  $ch = curl_init();
+	  curl_setopt_array($ch, array(
+	    CURLOPT_CONNECTTIMEOUT => $timeout ,
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => $url,
+		CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'] 
+	));
+	  
+	  
+   /*curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,10);
+   curl_setopt($ch,CURLOPT_HEADER,true);
+   curl_setopt($ch,CURLOPT_NOBODY,false);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   curl_setopt($ch,CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT'] );
+   curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+   curl_setopt($ch,CURLOPT_URL,$url);*/
+   //get answer
+   
+   $response = curl_exec($ch);
+   // Then, after your curl_exec call:
+	$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+	$header = substr($response, 0, $header_size);
+	$body = substr($response, $header_size);
+   $info = curl_getinfo($ch);
+   curl_close($ch);
+   
+   return $response;
+}
+
 ?>
