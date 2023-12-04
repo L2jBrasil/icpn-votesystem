225c13c22c3d2de3f771d05da520e5c58a2cc687
Saulo Germano <sgwebr@gmail.com>
2023-12-04 11:54:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 //       Brazillian Developer / WebSite: http://www.icpfree.com.br       \\
 //                 Email & Skype: ivan1507@gmail.com.br                  \\
 //=======================================================================\\
-include_once('../config/fix_mysql.inc.php');
 if (!isset($_SESSION)){ session_start(); }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN http://www.w3.org/TR/html4/strict.dtd">
