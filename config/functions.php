a1651d251c86862a9974ff1519997b14b2474439
Saulo Germano <sgwebr@gmail.com>
2023-12-20 00:02:25 -0300

    Update functions.php
    Fix mistake code on add 4top
    On first open, the script show error on:
    $db = strtolower($db_data) == "l2j" ? true : false;
    "dont have data in $db_data)"
    The error is over after put:
    if(file_exists("config/connect_config.php"))

--- config/functions.php
+++ config/functions.php
@@ -9,6 +9,7 @@
 //       Brazillian Developer / WebSite: http://www.icpfree.com.br       \\
 //                 Email & Skype: ivan1507@gmail.com.br                  \\
 //=======================================================================\\
+if(file_exists("config/connect_config.php"))
 $db = strtolower($db_data) == "l2j" ? true : false;
 
 function resposta($msg){
@@ -132,8 +133,8 @@ function instalar($db_ip, $db_user, $db_pass, $db_name, $db_data, $l2jruss, $adm
 		array(13, 'TopG', 'https://topg.org', 'topg.gif', 'topg.php', 'sem_id', 'sem_token', 0, 0),
 		array(14, 'GameBytes', 'https://www.gamebytes.net', 'gamebytes.png', 'gamebytes.php', 'sem_id', 'sem_token', 0, 0),
 		array(15, 'L2 Servers', 'https://www.l2servers.com', 'l2servers.png', 'l2servers.php', 'sem_id', 'sem_token', 0, 0),
-		array(16, 'L2 Votes', 'https://www.l2votes.com', 'l2votes.jpg', 'l2votes.php', 'sem_id', 'sem_token', 0, 0)
-		array(17, '4TOP Servers', 'https://top.4teambr.com', '4topmmo.png', '4topmmo.php', 'sem_id', 'sem_token', 0, 0),
+		array(16, 'L2 Votes', 'https://www.l2votes.com', 'l2votes.jpg', 'l2votes.php', 'sem_id', 'sem_token', 0, 0),
+		array(17, '4TOP Servers', 'https://top.4teambr.com', '4topmmo.png', '4topmmo.php', 'sem_id', 'sem_token', 0, 0)
 	);
 	if(empty($admins))
 		return respostaDelay($language_72,4000);
